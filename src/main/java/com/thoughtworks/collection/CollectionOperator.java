23b777b617bb4bf9019ee7f118e8ccf881331036
Narcissus <1169714441@qq.com>
2019-05-13 17:44:16 +0800

    完成CollectionOperator.popCommonElement(),通过测试

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,10 +2,12 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
@@ -25,7 +27,7 @@ public class CollectionOperator {
     }
 
     public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
+        return Arrays.stream(array).boxed().filter(e -> e % 2 == 0).collect(Collectors.toList());
     }
 
     public int popLastElment(int[] array) {
@@ -33,7 +35,7 @@ public class CollectionOperator {
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
+        return Arrays.stream(firstArray).boxed().filter(e -> Arrays.stream(secondArray).boxed().anyMatch(e1 -> e1.equals(e))).collect(Collectors.toList());
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
