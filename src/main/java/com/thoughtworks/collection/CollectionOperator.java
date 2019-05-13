e492c4755b567327e41ce7eaf5d4b34a31609899
Narcissus <1169714441@qq.com>
2019-05-13 18:56:13 +0800

    完成CollectionOperator.addUncommonElement(),通过测试

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -32,13 +33,18 @@ public class CollectionOperator {
 
     public int popLastElment(int[] array) {
         return array[array.length - 1];
-   }
+    }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
         return Arrays.stream(firstArray).boxed().filter(e -> Arrays.stream(secondArray).boxed().anyMatch(e1 -> e1.equals(e))).collect(Collectors.toList());
     }
 
     public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
+        List<Integer> first = Arrays.asList(firstArray);
+        List<Integer> second = Arrays.asList(secondArray);
+        List<Integer> result = new ArrayList<>();
+        result.addAll(Arrays.asList(firstArray));
+        result.addAll(second.stream().filter(e -> first.stream().noneMatch(e::equals)).collect(Collectors.toList()));
+        return result;
     }
 }
